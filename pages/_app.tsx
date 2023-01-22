90ef25d44f8249fd46901c7d41a2a24102c14a5a
Michaela <michaela.rudolfova@seznam.cz>
2023-01-22 16:12:27 +0100

    fix dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../src/components/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Footer from '../src/components/Footer';
 import MobileNavbar from '../src/components/MobileNavbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setDarkMode(prefersDark);
+    console.log(prefersDark);
+  }, []);
 
   return (
     <div className={darkMode ? 'dark' : ''}>
